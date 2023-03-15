341ea8e5dc37d79525908ae349cf6cf166e80179
GhozlaneSaouli <saouli.ghozlane@gmail.com>
2023-03-15 19:44:14 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $confirm_password = $_POST["confirm_password"];
+
+  header("Location: success.php");
+  exit();
+}
+?>
