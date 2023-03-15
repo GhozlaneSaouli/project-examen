412f713b5f5e84d9dfb69ce043bba1cc94f18702
GhozlaneSaouli <saouli.ghozlane@gmail.com>
2023-03-15 22:09:11 +0100

    create data_base

--- register.php
+++ register.php
@@ -1,11 +1,30 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["username"];
-  $email = $_POST["email"];
-  $password = $_POST["password"];
-  $confirm_password = $_POST["confirm_password"];
 
-  header("Location: success.php");
-  exit();
+try {
+      $bdd = new PDO('mysql:host=localhost;dbname=projectexam;charset=utf8','root', '');
+    //  echo "Connexion à la base de données réussie";
+  } catch (Exception $exc) {
+     // echo "Connexion à la base de données échouée";
+      //echo $exc->getTraceAsString();
+  }
+
+$username= $_POST['username'];
+
+$email = $_POST['email'];
+$password = $_POST["password"];
+$confirm_password = $_POST['confirm_password'];
+
+echo "$username $email $password $confirm_password";
+
+
+
+
+
+if($password == $confirm_password){
+ 
+    $inserer = $bdd->query("INSERT INTO connection(username,email,password,confirm_password) VALUES ('$username','$email', '$password', '$confirm_password')");//POUR INSIRER LES VALUE DANS LES ATRIBUE QUI EXISTE DANS LE TABLEAU
+
 }
+
+
 ?>
